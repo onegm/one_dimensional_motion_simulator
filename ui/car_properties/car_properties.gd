26af084891dd01262e4b3f1d44a025fee95bd7b1
Omar - Home <negmok@gmail.com>
2024-09-28 08:45:50 -0500

    Add vsplit grabber icon and car property frames

--- ui/car_properties/car_properties.gd
+++ ui/car_properties/car_properties.gd
@@ -1,15 +1,18 @@
-extends VBoxContainer
+extends Control
 
 @export var h_flip : bool
 
 var car : Car:
 	set(new_car):
 		car = new_car
+		self_modulate = car.color
 		connect_signals()
 
 @onready var position_controller := %PositionController
 @onready var velocity_controller := %VelocityController
 @onready var acceleration_controller := %AccelerationController
+
+@onready var controllers := [position_controller, velocity_controller, acceleration_controller]
 
 func connect_signals() -> void:
 	position_controller.value_changed.connect(car.set_initial_position)
@@ -18,8 +21,6 @@ func connect_signals() -> void:
 
 func _ready() -> void:
 	if !h_flip: return
-	for row in get_children():
-		if row is not HBoxContainer:
-			continue
+	for row in controllers:
 		row.move_child(row.get_child(0), -1)
 		
